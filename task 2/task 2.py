434e70501284d8d2d3991f2278073ec5e6137692
avgdrainer <123040359+avgdrainer@users.noreply.github.com>
2024-04-08 12:47:54 -0600

    Update task 2.py

--- task 2/task 2.py
+++ task 2/task 2.py
@@ -1,6 +1,8 @@
+import requests, bs4, openpyxl
+
 class Author:
   contributions = {"2021": 0, "2022": 0, "2023": 0}
-  total = contributions["2021"] + contributions["2022"] + contributions["2023"]
+  total = sum(contributions.values())
 
   def __init__(self, name):
       self.name = name
@@ -8,13 +10,11 @@ class Author:
   def tally(self, year):
       contributions[year] += 1
 
-#ISSUES
-#what if the same author publishes in different year? how to not create a new object
 
 
 def main():
 
-  authors = []
+  authors = {} #"name": object maybe
   urls[]
   
   for url in urls:
@@ -27,18 +27,36 @@ def main():
 
 #creates a BeautifulSoup object, scans for all contributors, places in list
 #scrape the year of the website
-def getNames(url, year)
-
+def getNames(url, year):
 
 #takes list of names, for each unique name creates an author object, adds to author list
 # for each instance of that name in the list, increment Author.contributions, del list element
-def countNames(names, authors, year)
+def countNames(names, authors, year):
+
+  for name in names:
+    if name in authors:
+      authors[name].tally(year)
+    else:
+      authors[name] = Author(name)
+      authors[name].tally(year)
 
 #iteratres through authors[] to find top three, del all other authors
-def topContributors(authors)
+def topContributors(authors):
+
+  
+  if len(authors) < 3:
+    print("not enough contributors for a top three. exiting program...")
+    exit()
+  elif len(authors) == 3:
+    return authors:
+  else:
+    for author in authors:
+      #use sort on list(authors.values()), something like sort(list(authors.values()), key = element.total)
+      
+    
 
 #prints the top three authors' number of contributions in an Excel spreadsheet
-def sendExcel(authors)
+def sendExcel(authors):
 
 
 
