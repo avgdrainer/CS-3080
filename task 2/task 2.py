cca754cdb72deeba2da3aec83bd35ff25e3bee63
avgdrainer <rowens1255@gmail.com>
2024-04-14 16:35:43 -0600

    completed py file, pushing related excel file and report

--- task 2/task 2.py
+++ task 2/task 2.py
@@ -1,46 +1,69 @@
-import requests, bs4, openpyxl
+import requests, bs4, openpyxl, re, time
+from openpyxl.utils import get_column_letter
 
 class Author:
-  contributions = {"2021": 0, "2022": 0, "2023": 0}
-  total = sum(contributions.values())
 
   def __init__(self, name):
       self.name = name
+      self.contributions = {"2021": 0, "2022": 0, "2023": 0}
     
   def tally(self, year):
-      contributions[year] += 1
+      self.contributions[year] += 1
+
+  def sum(self):
+    return sum(self.contributions.values())
 
 
 
 def main():
 
-  authors = {} #"name": object maybe
-  urls[]
-  
-  for url in urls:
-    year = 0
-    names = getNames(url, year)
-    countNames (names, year, authors)
+  authors = {} 
+  urls = [r'https://openaccess.thecvf.com/CVPR2021?day=all', r'https://openaccess.thecvf.com/CVPR2022?day=all', r'https://openaccess.thecvf.com/CVPR2023?day=all']
 
-  topContributors(authors)
-  sendExcel(authors)
+  for url in urls:
+    
+    year = getYear(url)
+    names = getNames(url, year)
+    countNames (names, authors, year)
+
+  topThree = topContributors(authors)
+  sendExcel(topThree)
+
+#uses pattern matching to determine what year we are looking at
+def getYear(url):
+
+  pattern = re.compile(r'\d{4}')
+  year = pattern.search(url)
+
+  return year.group()
 
 #creates a BeautifulSoup object, scans for all contributors, places in list
-#scrape the year of the website
 def getNames(url, year):
 
+  res = requests.get(url)
+
+  res.raise_for_status()
+
+  bs4_object = bs4.BeautifulSoup(res.text, 'html.parser')
+  names = bs4_object.select('input[name="query_author"]')  
+
+  return names
+
 #takes list of names, for each unique name creates an author object, adds to author list
-# for each instance of that name in the list, increment Author.contributions, del list element
+# for each instance of that name in the list, increment Author.contributions, del name
 def countNames(names, authors, year):
 
-  for name in names:
-    if name in authors:
-      authors[name].tally(year)
-    else:
-      authors[name] = Author(name)
-      authors[name].tally(year)
+   for name in names: #for x in range(12):
 
-#iteratres through authors[] to find top three, del all other authors
+    value = name.get('value')
+
+    if value in authors:
+      authors[value].tally(year)
+    else:
+      authors[value] = Author(value)
+      authors[value].tally(year)
+
+#iterates through authors dictionary values to find top three, return list of corresponding Author objects 
 def topContributors(authors):
 
   
@@ -48,19 +71,34 @@ def topContributors(authors):
     print("not enough contributors for a top three. exiting program...")
     exit()
   elif len(authors) == 3:
-    return authors:
+    return authors
   else:
-    for author in authors:
-      #use sort on list(authors.values()), something like sort(list(authors.values()), key = element.total)
+    authorObjects = list(authors.values())
+    authorObjects.sort(key=lambda x: x.sum(), reverse=True)
+    authorObjects = authorObjects[:3]
+    return authorObjects     
       
-    
-
 #prints the top three authors' number of contributions in an Excel spreadsheet
-def sendExcel(authors):
+def sendExcel(topThree):
+ 
+  wb = openpyxl.Workbook()
+  sheet = wb.active
 
+  sheet['A2'] = "2021"
+  sheet['A3'] = "2022"
+  sheet['A4'] = "2023"
+  sheet['A5'] = "Total"
 
+  for x in range(0, 3):
+      
+    column = get_column_letter(2 + x)
+    sheet[column + '1'] = topThree[x].name
 
+    for y in range(2, 5):
+      sheet[column + f'{y}'] = topThree[x].contributions[sheet['A' + f'{y}'].value]
+    sheet[column + '5'] = topThree[x].sum()
 
+  wb.save(r'C:\Users\Riley\Documents\GitHub\CS-3080\task 2\top three.xlsx')
 
 
 main()
